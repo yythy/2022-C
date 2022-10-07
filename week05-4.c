9a202a540a509489b8ceb9c802f62d5e8aab29cb
yythy <114201409+yythy@users.noreply.github.com>
2022-10-07 11:12:10 +0800

    Add files via upload

---
+++ week05-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<5 ;i++)
+    {
+        printf("電腦從0開始數,i:%d\n",i);
+    }
+     for(int i=1; i<=5 ;i++)
+    {
+        printf("人從1開始數,i:%d\n",i);
+    }
+    for(int j=5; j>0 ;j--)
+    {
+        printf("倒過來的迴圈,j:%d\n",j);
+    }
+}
